7b58636413b91a17941c8c787374d526d1e27b52
Goodluck Ogundare <155663393+Tim-cryptow@users.noreply.github.com>
2025-01-22 11:24:33 +0100

    Update predict.py

--- src/predict.py
+++ src/predict.py
@@ -3,8 +3,10 @@ import joblib
 import pandas as pd
 import json
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 # Loading the model and associated files
 try:
